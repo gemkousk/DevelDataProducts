9f48286582f7910c09349a1afc72ac8fd405fcd2
Koushik Mondal <gemkousk@gmail.com>
2014-10-20 21:26:05 +0530

    Project1
    Updated1

---
+++ server.R
@@ -0,0 +1,27 @@
+
+# This is the server logic for a Shiny web application.
+library(shiny)
+library(ggplot2)
+
+shinyServer(function(input, output) {
+  
+  output$plot <- renderPlot({
+    col <- mtcars[input$color][,1]
+    p <- qplot(mtcars[input$x][,1], mpg, data=mtcars, geom=c("point", "smooth"), 
+               method="lm", formula=y~x, color=col, 
+               main="Linear Regression (OLS) Fit", 
+               xlab=input$x, ylab="mpg")
+    
+    print(p)
+  })
+  datasetInput <- reactive({
+    switch(input$var, mtcars[input$var])
+  })
+  output$summary <- renderPrint({
+    dataset <- datasetInput()
+    summary(dataset)
+  })
+  output$view <- renderTable({
+    head(datasetInput(), n = input$obs)
+  })
+})
